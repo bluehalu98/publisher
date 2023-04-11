50d8d543579a0567e1a153f0d18f36f99da85833
진영진 <jin@seiim.co.kr>
2023-04-11 15:38:30 +0900

    no message

--- 31~40/38/js/ex.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-  $('.ex8 button').click(function () {
-    var n = $(this).attr('data-n')
-    var color = $(this).attr('data-color')
-    $('.ex8 button:not(.btn' + n + ')').removeClass('red blue green yellow purple')
-    $('.ex8 button:nth-child(' + n + ')').toggleClass(color)
-  })
-})
